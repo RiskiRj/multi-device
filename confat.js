ce5cef261a76f68172c9ff36d547ca1d89a8c225
RiskiRj <riskirodja76@gmail.com>
2022-03-01 10:49:15 +0800

    multi-device

---
+++ confat.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Other
+global.owner = ["6281339888334","+6283842963033"]
+global.packname = 'RiskiGanz'
+global.youtube = 'https://youtube.com/channel/UCc5cixXT3efHmoFsRxs7htA'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh RiskiGanz!',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Yui',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh RZ-MD!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+global.thumb = fs.readFileSync('./image/lol.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
